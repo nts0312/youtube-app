cb290b87511f24225de2a6aeb9e48f33727d32b8
nts0312 <nitinsinghm111@gmail.com>
2024-05-05 02:44:47 +0530

    icons added in side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Button from "./Button";
+import subs from "./../assets/subs.svg";
+import shorts from "./../assets/shorts.svg";
+import home from "./../assets/home.svg";
 
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
@@ -9,12 +11,27 @@ const Sidebar = () => {
     return null;
   }
 
+  const list = [
+    { name: "Home", img: home },
+    { name: "Shorts", img: shorts },
+    { name: "Subscriptions", img: subs },
+  ];
+
   return (
     <div className="">
-      <div className=" rounded-md w-52 flex cursor-pointer ml-3 h-11 m-2 py-1.5 bg-custom-gray">
-        <h1 className="ml-4">🏠</h1>
+      {list.map((item, index) => (
+        <div
+          key={index}
+          className=" rounded-md w-52 flex cursor-pointer ml-3 h-11 m-2 py-1.5 bg-custom-gray"
+        >
+          <img className="ml-4 w-5 pb-1" src={item.img} alt={item.name} />
+          <h1 className="ml-6 pt-1 font-semibold text-sm">{item.name}</h1>
+        </div>
+      ))}
+      {/* <div className=" rounded-md w-52 flex cursor-pointer ml-3 h-11 m-2 py-1.5 bg-custom-gray">
+        <img className="ml-4" />
         <h1 className="ml-6 pt-1 font-semibold text-sm">Home</h1>
-      </div>
+      </div> */}
       {/* <ul className="">
         <li className="ml-0">Music</li>
         <li>Sports</li>
